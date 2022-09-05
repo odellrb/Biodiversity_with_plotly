a78fd2f7b315217e776da3419b8705236a54bc34
Richard B Odell Jr <richardbodell0014@gmail.com>
2022-09-04 23:15:49 -0400

    Add files via upload

--- static/js/charts.js
+++ static/js/charts.js
@@ -106,6 +106,40 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar",barData, barLayout)
+
+    // start of bubble chart
+    
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+   
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title:`All Bacteria Cultures Per Sample ${sample}`,
+      showlegend: false,
+      xaxis:{title: "OTU ID"},
+      yaxis:{title: "Bacteria Count"},
+      
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+
   });
+  // sample.json no longer available
 }
-// doggy
+
