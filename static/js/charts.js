d78a02dab7cfab6ba885dfb9956c0c2b5d8f59de
Richard B Odell Jr <richardbodell0014@gmail.com>
2022-09-05 00:23:14 -0400

    Add files via upload

--- static/js/charts.js
+++ static/js/charts.js
@@ -97,14 +97,14 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse()
+    var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
     // console.log(yticks);
 
     // 8. Create the trace for the bar chart. 
     var barData = [
       {
+        x: sample_values.slice(0,10).reverse(),
         y: yticks,
-        x: sample_values,
         text: otu_labels,
         type: "bar",
         orientation: "h",
@@ -113,8 +113,13 @@ function buildCharts(sample) {
     ];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-      title: `Top 10 Bacteria Cultures Found in Sample`
-
+      title: `Top 10 Bacteria Cultures Found in Sample`,
+      margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b:100
+      }
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout)
